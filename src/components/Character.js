73071301e97e3f9fdf46f9f0424611661fa18a79
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2022-12-15 10:45:26 -0600

    created the Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledH2 = styled.h2`
+    font-size: 2.25rem;
+    background: gray;
+    border: 3px solid black;
+    padding: 3%;
+    border-radius: 3px;
+    &:hover {
+        background: white;
+    }
+`
+
+function Character(props) {
+    return (
+        <>
+            <StyledH2 onClick={() => props.action(props.props.key)}>{props.props.name}</StyledH2>
+        </>
+    )
+}
+
+export default Character;
